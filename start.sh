926db5fa2a6faf01face98969d080ec1b8169f41
gunapriya07 <gunapriya77776@gmail.com>
2026-05-05 12:37:24 +0530

    update environment configuration and enhance start script for deployment

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
+chmod -R 775 storage bootstrap/cache
+
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
-
 php artisan config:cache
 
 php artisan migrate --force
-
-# Fix permissions
-chmod -R 775 storage bootstrap/cache
 
 php-fpm &
 nginx -g "daemon off;"
